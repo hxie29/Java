37736531235d76b417e33b3ad2db66ba7f624649
haldax <74546595+haldax@users.noreply.github.com>
2022-04-24 18:29:24 +0200

    x

---
+++ IntroJava_Chap_5-6/E0506.java
@@ -0,0 +1,20 @@
+public class E0506 {
+    public static void main(String[] args) {
+        double tuition = 10000;
+        int year = 0;
+        double sum = 0;
+        double rate = 1 + 0.06;
+
+        while (year <10) {
+            year++;
+            tuition*=rate;
+            System.out.println("The tuition of year " + year + " is $" + (int)(tuition * 100)/100.0);
+        }
+        while (year <14){
+            year++;
+            tuition*=rate;
+            sum+=tuition;
+        }
+        System.out.printf("%s%.3f", "The total cost of four years tuition after the tenth year is $", sum);
+    }
+}
---
+++ IntroJava_Chap_5-6/E05L14A.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class E05L14A {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String word = input.nextLine();
+
+        int high = word.length() - 1;
+        int low = 0;
+
+        boolean isPandrome = true;
+        while (low < high) {
+            if (word.charAt(low) != word.charAt(high)) {
+                isPandrome = false;
+                break;
+            }
+            high--;
+            low++;
+        }
+        if (isPandrome) {
+            System.out.println(word + " is palindrom.");
+        }
+        else {
+            System.out.println(word + " is not a palindrom.");
+        }
+        input.close();
+    }    
+}
