515a5c0b71b3d01f69b196534d3a182c6ce79976
haldax <74546595+haldax@users.noreply.github.com>
2022-04-25 20:00:40 +0200

    x

---
+++ IntroJava_Chap_5-6/E0550.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class E0550 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter a string");
+        String sen = input.nextLine();
+
+        for (int n = 1; n <= 9; n++) {
+            for (int k =1; k <= 9; k++) {
+                System.out.print(k + "*" + n + "= " + (k * n) + "\t");
+            } 
+            System.out.println();
+        }
+        input.close();
+    }
+}
