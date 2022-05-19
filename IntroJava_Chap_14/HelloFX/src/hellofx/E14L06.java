8473914f748f0412176ddad10d1b1c17a250934b
haldax <74546595+haldax@users.noreply.github.com>
2022-05-19 15:12:55 +0200

    x

--- IntroJava_Chap_14/HelloFX/src/hellofx/E14L06.java
+++ IntroJava_Chap_14/HelloFX/src/hellofx/E14L06.java
@@ -7,8 +7,8 @@ public class E14L06{
     public static void main(String[] args) {
         DoubleProperty d1 = new SimpleDoubleProperty(1);
         DoubleProperty d2 = new SimpleDoubleProperty(2);
-        d1.bind(d2);
         System.out.println("d1 is " + d1.getValue() + " d2 is " + d2.getValue());
+        d1.bind(d2);
         d2.set(70);
         System.out.println("d1 is " + d1.getValue() + " d2 is " + d2.getValue());
     }
